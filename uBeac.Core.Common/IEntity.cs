517a63b48ee4c987b017cf3572c8d7f4222cd113
Amir Pournasserian <amir.pournasserian@live.com>
2021-04-15 17:26:13 -0400

    Adding uBeac.Core.Common

---
+++ uBeac.Core.Common/IEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uBeac.Core.Common
+{
+    public interface IEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+    }
+
+    public interface IEntity : IEntity<Guid>
+    {
+    }
+
+    public class Entity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class Entity : Entity<Guid>, IEntity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
