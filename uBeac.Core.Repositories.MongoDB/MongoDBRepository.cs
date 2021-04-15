277a5c08ed6e08540ad23b027afc490048ce6d07
Amir Pournasserian <amir.pournasserian@live.com>
2021-04-15 17:45:49 -0400

    Adding uBeac.Core.Repositories.MongoDB

---
+++ uBeac.Core.Repositories.MongoDB/MongoDBRepository.cs
@@ -0,0 +1,100 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using uBeac.Core.Common;
+using uBeac.Core.Repositories.Abstractions;
+
+namespace uBeac.Core.Repositories.MongoDB
+{
+    public class MongoDBRepository<TKey, TEntity> : IEntityRepository<TKey, TEntity>
+         where TKey : IEquatable<TKey>
+         where TEntity : IEntity<TKey>
+    {
+        protected readonly IMongoCollection<TEntity> Collection;
+        protected readonly IMongoCollection<BsonDocument> BsonCollection;
+        protected readonly IMongoDatabase MongoDatabase;
+
+        public MongoDBRepository(IMongoDatabase mongoDatabase)
+        {
+            MongoDatabase = mongoDatabase;
+            Collection = mongoDatabase.GetCollection<TEntity>(GetCollectionName());
+            BsonCollection = mongoDatabase.GetCollection<BsonDocument>(GetCollectionName());
+        }
+
+        protected virtual string GetCollectionName()
+        {
+            return typeof(TEntity).Name;
+        }
+
+        protected virtual FilterDefinition<TEntity> GetIdFilter(TKey id)
+        {
+            return Builders<TEntity>.Filter.Eq("_id", id);
+        }
+
+        protected virtual FilterDefinition<TEntity> GetIdsFilter(IEnumerable<TKey> ids)
+        {
+            return Builders<TEntity>.Filter.In("_id", ids);
+        }
+
+        public async Task<bool> Delete(TKey id, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var idFilter = GetIdFilter(id);
+            var deleteResult = await Collection.DeleteOneAsync(idFilter, cancellationToken);
+            return deleteResult.DeletedCount == 1;
+        }
+
+        public async Task<long> DeleteMany(IEnumerable<TKey> ids, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var idsFilter = GetIdsFilter(ids);
+            var deleteResult = await Collection.DeleteManyAsync(idsFilter, cancellationToken);
+            return deleteResult.DeletedCount;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var filter = Builders<TEntity>.Filter.Empty;
+            return (await Collection.FindAsync(filter, null, cancellationToken)).ToEnumerable();
+        }
+
+        public async Task<TEntity> GetById(TKey id, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var idFilter = GetIdFilter(id);
+            return (await Collection.FindAsync(idFilter, null, cancellationToken)).FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetByIds(IEnumerable<TKey> ids, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var idsFilter = GetIdsFilter(ids);
+            return (await Collection.FindAsync(idsFilter, null, cancellationToken)).ToEnumerable();
+        }
+
+        public async Task Insert(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            await Collection.InsertOneAsync(entity, null, cancellationToken);
+        }
+
+        public async Task InsertMany(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            await Collection.InsertManyAsync(entities, null, cancellationToken);
+        }
+
+        public virtual Task<bool> Update(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            // todo: implement here
+            cancellationToken.ThrowIfCancellationRequested();
+            throw new NotImplementedException();
+        }
+
+    }
+}
