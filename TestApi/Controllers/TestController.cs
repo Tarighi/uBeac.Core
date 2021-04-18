8d9442f6def5d153d8b86c72dd143f4ba5bdb4dc
Amir Pournasserian <amir.pournasserian@live.com>
2021-04-18 11:13:20 -0400

    Adding TestController to test authorization

--- TestApi/Controllers/TestController.cs
+++ TestApi/Controllers/TestController.cs
@@ -1,66 +1,31 @@
-﻿//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using uBeac.Core.Identity;
-//using uBeac.Core.Web;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace TestApi.Controllers
-//{
-//    public class TestController : BaseController
-//    {
-//        private readonly RoleManager<Role> _roleManager;
-//        private readonly UserManager<User> _userManager;
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]/[action]/")]
+    [ApiController]
+    public class TestController
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public TestController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
 
-//        public TestController(RoleManager<Role> roleManager, UserManager<User> userManager)
-//        {
-//            _roleManager = roleManager;
-//            _userManager = userManager;
-//        }
+        [HttpGet]
+        public string TestA()
+        {
+            return _httpContextAccessor.HttpContext.User.Identity.Name;
+        }
 
-//        [Get]
-//        public async Task<IResultSet> TestRoles(CancellationToken cancellationToken = default)
-//        {
-//            for (int i = 0; i < 10; i++)
-//            {
-//                var role = new Role { Name = "Role_" + i.ToString() };
-//                await _roleManager.CreateAsync(role);
-//                await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("a", "b"));
-//                await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("c", "d"));
-//            }
-
-//            var x = await _roleManager.FindByNameAsync("role_1");
-//            var y = await _roleManager.GetClaimsAsync(x);
-//            var z = await _roleManager.FindByIdAsync(x.Id.ToString());
-//            var k = _roleManager.Roles.ToList();
-
-//            return "OK".ToResultSet();
-//        }
-
-//        [Get]
-//        public async Task<IResultSet> TestUsers(CancellationToken cancellationToken = default)
-//        {
-//            var result = new List<IdentityResult>();
-//            for (int i = 0; i < 2; i++)
-//            {
-//                var user = new User
-//                {
-//                    Email = "aaa_" + i.ToString() + "@xxx.com",
-//                    UserName = "aaa_" + i.ToString(),
-//                };
-//                var idResult = await _userManager.CreateAsync(user, "password_" + i.ToString());
-//                result.Add(idResult);
-
-//                if (idResult.Succeeded)
-//                {
-//                }
-//            }
-
-
-//            return result.ToResultSet();
-//        }
-
-//    }
-//}
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public string TestB()
+        {
+            return _httpContextAccessor.HttpContext.User.Identity.Name;
+        }
+    }
+}
