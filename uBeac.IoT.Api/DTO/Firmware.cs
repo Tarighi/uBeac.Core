b0bae5ad6c781593c3271e2744a549c3a8f8b15c
Amir Pournasserian <ap@momentaj.com>
2021-04-27 18:10:14 -0400

    Adding model validation for Manufacturer, Product, Firmware

--- uBeac.IoT.Api/DTO/Firmware.cs
+++ uBeac.IoT.Api/DTO/Firmware.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using uBeac.IoT.Models;
 
 namespace uBeac.IoT.Api.DTO
 {
     public class FirmwareAddRequest
     {
+        [Required]
         public Guid ProductId { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
         public string Name { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
+
+        [Required]
         public int ViewOrder { get; set; }
     }
 
     public class FirmwareUpdateRequest: FirmwareAddRequest
     {
+        [Required]
         public Guid Id { get; set; }
     }
 
